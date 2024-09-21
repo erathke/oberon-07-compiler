d8ae1a5689f73bb16e1bafd4e389544892c98c07
Evandro Rathke <evandro@erdev.tech>
2024-09-21 19:08:18 -0300

    samples: assert without stdlib dependency

---
+++ samples/linux32o/assert/build.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+CC="cc"
+CFLAGS="-m32 -no-pie"
+COPTIONS="-nostartfiles -nostdlib"
+#COPTIONS="-nostartfiles"
+
+../../../compilerX Assert.ob07 linux32o -def API_DEBUG && $CC $CFLAGS $COPTIONS -o Assert Assert.xo -Wl,-T./min.ld,--build-id=none
+#../../../compilerX Assert.ob07 linux32o -def API_DEBUG && $CC $CFLAGS $COPTIONS -o Assert Assert.xo
+#../../../compilerX Assert.ob07 linux32o && ld -T./min.ld Assert.xo -o Assert
+#../../../compilerX Assert.ob07 linux32o  && $CC $CFLAGS $COPTIONS -o Assert Assert.xo
---
+++ samples/linux32o/assert/ld.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+LD=ld
+$LD -T./min.ld Assert.xo -o Assert
