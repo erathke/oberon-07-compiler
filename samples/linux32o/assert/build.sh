378c38e8d86cccb426f3c872c630a7966a276674
Evandro Rathke <erathke@gmail.com>
2024-09-22 13:14:36 -0300

    samples: tests

--- samples/linux32o/assert/build.sh
+++ samples/linux32o/assert/build.sh
@@ -4,8 +4,9 @@ CC="cc"
 CFLAGS="-m32 -no-pie"
 COPTIONS="-nostartfiles -nostdlib"
 #COPTIONS="-nostartfiles"
+#DEF="-def API_DEBUG"
 
-../../../compilerX Assert.ob07 linux32o -def API_DEBUG && $CC $CFLAGS $COPTIONS -o Assert Assert.xo -Wl,-T./min.ld,--build-id=none
+../../../compilerX Assert.ob07 linux32o $DEF && $CC $CFLAGS $COPTIONS -o Assert Assert.xo -Wl,-T./min.ld,--build-id=none
 #../../../compilerX Assert.ob07 linux32o -def API_DEBUG && $CC $CFLAGS $COPTIONS -o Assert Assert.xo
 #../../../compilerX Assert.ob07 linux32o && ld -T./min.ld Assert.xo -o Assert
 #../../../compilerX Assert.ob07 linux32o  && $CC $CFLAGS $COPTIONS -o Assert Assert.xo
