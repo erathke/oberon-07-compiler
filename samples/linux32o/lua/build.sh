c2cecb87638e1d963c00918ca2bf52cd7adcb726
Evandro Rathke <erathke@gmail.com>
2024-09-24 00:12:03 -0300

    samples: new extern syntax

--- samples/linux32o/lua/build.sh
+++ samples/linux32o/lua/build.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 
 CC="gcc"
+#CC="i686-linux-gnu-gcc"
 CFLAGS="-m32 -no-pie"
-COPTIONS="-nostartfiles"
+COPTIONS="-nostartfiles -Wl,--gc-sections"
+#COPTIONS="-static -nostartfiles -Wl,--gc-sections"
 LIBS="liblua-i686-posix.a"
 #LIBS="liblua-i686-linux.a"
-#DEF="-def API_DEBUG"
-#OPTS="-stk 1"
+DEF="-def NO_STARTFILES"
+OPTS="-stk 1"
 
 mkdir -p bin;
 ../../../compilerX $1.ob07 linux32o $DEF $OPTS && 
