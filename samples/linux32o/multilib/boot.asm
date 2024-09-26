1d7e2eaf8b7941d4a9970334ca6fa5f4fabe1ad7
Evandro Rathke <evandro@erdev.tech>
2024-09-26 16:00:32 -0300

    samples: testing memory

--- samples/linux32o/multilib/boot.asm
+++ samples/linux32o/multilib/boot.asm
@@ -10,25 +10,44 @@ extrn kernel_config
 
 SYS_write = 4
 STDOUT = 1
-MEM_SIZE = 10
+MEM_SIZE = 64
 
 _start:
+	
+	; initial message
+	push stmsg_size
+	push stmsg
+	call print
+
+	; calling kernel_config(mem, MEM_SIZE)
+	push MEM_SIZE
 	push mem
 	call kernel_config
-	call print
-	call kernel_main
-	ret
+	
+	; calling kernel_main()
+	call kernel_main ; long jump
+	
+	ret ; should not be executed
+
 print:
+	push ebp
+	mov ebp, esp
 	mov eax, SYS_write
 	mov ebx, STDOUT
-	mov ecx, mem
-	mov edx, MEM_SIZE
+	mov ecx, [ebp+8]
+	mov edx, [ebp+12]
 	int 80h
-	ret
+	pop ebp
+	ret 8 ; poping 8 bytes from stack
+
 
 section '.data' writeable
+stmsg db "Booting OS...", 10
+stmsg_size = $ - stmsg
+
 
 section '.bss' writeable
 mem rb MEM_SIZE
+
 
 section '.note.GNU-stack'
