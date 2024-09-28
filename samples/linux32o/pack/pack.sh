071640974c56913c6dc354b117c63c7b5a8725a3
Evandro Rathke <evandro@erdev.tech>
2024-09-28 20:12:21 -0300

    samples: pack with embedded run.sh

--- samples/linux32o/pack/pack.sh
+++ samples/linux32o/pack/pack.sh
@@ -1,10 +1,64 @@
 #!/bin/bash
 
-ZIP="$1.zip"
+if [[ $# -lt 2 ]] ; then
+	echo "Usage: $0 <name> <folder>"
+	exit 1
+fi
+
 FOLDER="$2"
+if [[ ! -d $FOLDER ]]; then
+	echo "Invalid folder: $2"
+	exit 1
+fi
 
-zip -j $ZIP $FOLDER 
-cat deliver.sh $ZIP > $1  && chmod +x $1
+NAME="$1"
+EXEC="$FOLDER/$NAME"
+if [[ ! -f $EXEC ]]; then
+	echo "Invalid exec name: $NAME"
+	exit 1
+fi
 
+read -r -d '' "RUN_SCRIPT" << "EOF"
+#!/bin/bash
+WORKDIR=$( mktemp -d )
+ 
+#find last line +1
+SCRIPT_END=$( awk '
+  BEGIN { err=1; } 
+  /^\w*___END_OF_SHELL_SCRIPT___\w*$/ { print NR+1; err=0; exit 0; } 
+  END { if (err==1) print "?"; }
+' "$0" )
+
+# check for error
+if [ "$SCRIPT_END" == '?' ]
+then
+   echo "Can't find embedded file"
+   exit 1
+fi
+
+# Extract file
+tail -n +$SCRIPT_END $0 >"$WORKDIR/file"
+
+# Do something with the file
+unzip -q "$WORKDIR/file" -d "$WORKDIR" && cd "$WORKDIR" && exec "$0" "${@:1}" &
+
+# Wait for finishing the execution
+trap ' ' INT
+wait $!
+
+#  Cleanup and exit
+rm -r "$WORKDIR"
 exit 0
 
+# Here's the end of the script followed by the embedded file
+___END_OF_SHELL_SCRIPT___
+
+EOF
+
+ZIP="$NAME.zip"
+zip -q -j $ZIP $FOLDER/* && echo "Compressing done." || exit 1
+echo "$RUN_SCRIPT" > $NAME && cat $ZIP >> $NAME && chmod +x $NAME
+echo "Executable file created: $NAME"
+
+rm $ZIP
+exit 0
