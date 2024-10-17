d36587933403fdcf5bf6617497d9b2ab320b8c2e
Evandro Rathke <evandro@erdev.tech>
2024-10-17 14:32:42 -0300

    samples: kernel simple tasks

--- samples/linux32o/kernel/loader.asm
+++ samples/linux32o/kernel/loader.asm
@@ -26,8 +26,6 @@ section '.text' executable
 	public reload_segs
 	public _stop
 	public set_handlers
-	public setjmp
-	public longjmp
 	
 	extrn kernelSetup
 	extrn kernelMain
@@ -70,9 +68,6 @@ flush_cs:
 ; Interrupts
 include 'interrupts.asm'
 
-
-; Coroutines
-include 'coroutines.asm'
 
 section '.data'
 bye db 'Bye!',0
