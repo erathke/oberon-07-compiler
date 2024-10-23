176998035875b26d383c0064438135abc4e61e10
Evandro Rathke <evandro@erdev.tech>
2024-10-23 13:00:06 -0300

    samples: coroutines in progress

--- samples/linux32o/coro/coroutines.asm
+++ samples/linux32o/coro/coroutines.asm
@@ -4,38 +4,46 @@ section '.text' executable
 
 public getcontext
 getcontext: ; ctx1
-	; ebx, esi, edi, ebp, ecx, esp, eip
-	mov eax, [esp + 4]		;mov 4(%esp), %eax
-	mov [eax +  0], ebx			;mov    %ebx, (%eax)
-	mov [eax +  4], esi		;mov    %esi, 4(%eax)
-	mov [eax +  8], edi		;mov    %edi, 8(%eax)
-	mov [eax + 12], ebp		;mov    %ebp, 12(%eax)
-	lea ecx, [esp + 4]		;lea 4(%esp), %ecx
-	mov [eax + 16], ecx		;mov    %ecx, 16(%eax)
-	mov ecx, [esp]			;mov  (%esp), %ecx
-	mov [eax + 20], ecx		;mov    %ecx, 20(%eax)
+	; eip, esp, ebp, ebx, ecx, edi, esi
+	mov eax, [esp + 4]
+	mov [eax +  4], esp ; esp
+	mov [eax +  8], ebp ; ebp
+	mov [eax + 12], ebx ; ebx
+	mov [eax + 16], ecx	; ecx
+	mov [eax + 20], edi ; edi
+	mov [eax + 24], esi ; esi
+	
+	;mov eax, [esp + 4]
+	;mov ecx, [esp]
+	;mov [eax +  4], ecx ; esp
+	;mov [eax +  8], ebp ; ebp
+	;mov [eax + 12], ebx ; ebx
+	;lea ecx, [esp + 4]
+	;mov [eax + 16], ecx	; ecx
+	;mov [eax + 20], edi ; edi
+	;mov [eax + 24], esi ; esi
 	ret
 
 
 public swapcontext
 swapcontext: ; ctx1, ctx2
-	; ebx, esi, edi, ebp, ecx, esp, eip
-	mov eax, [esp + 4]		;movl 4(%esp), %eax
-	mov edx, [esp + 8]		;movl 8(%esp), %edx
-	mov [eax +  0], ebx		;movl %ebx, (%eax)
-	mov [eax +  4], esi		;movl %esi, 4(%eax)
-	mov [eax +  8], edi		;movl %edi, 8(%eax)
-	mov [eax + 12], ebp		;movl %ebp, 12(%eax)
-	mov [ecx + 16], ecx		;movl %ecx, 16(%eax)
-	mov [eax + 20], esp		;movl %esp, 20(%eax)
-	mov esp, [edx + 20]		;movl 20(%edx), %esp
-	mov ecx, [edx + 16]		;movl 16(%edx), %ecx
-	mov ebp, [edx + 12]		;movl 12(%edx), %ebp
-	mov edi, [edx +  8]		;movl 8(%edx), %edi
-	mov esi, [edx +  4];	movl 4(%edx), %esp
-	mov ebx, [edx +  0];	movl 4(%edx), %esp
-	jmp dword [edx + 24]
-	;ret
+	; eip, esp, ebp, ebx, ecx, edi, esi
+	mov eax, [esp + 4]
+	mov edx, [esp + 8]
+	mov [eax +  4], esp
+	mov [eax +  8], ebp
+	mov [eax + 12], ebx
+	mov [eax + 16], ecx
+	mov [eax + 20], edi
+	mov [eax + 24], esi
+	mov esi, [edx + 24]
+	mov edi, [edx + 20]
+	mov ecx, [edx + 16]
+	mov ebx, [edx + 12]
+	mov ebp, [edx +  8]
+	mov esp, [edx +  4]
+	jmp dword [edx]
+	ret
 
 
 section '.note.GNU-stack'
