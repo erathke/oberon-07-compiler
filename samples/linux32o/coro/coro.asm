099b7d6b530f5df3bcb72cfee5b39e4b1af52462
Evandro Rathke <evandro@erdev.tech>
2024-10-24 13:40:37 -0300

    samples: coroutines stack

--- samples/linux32o/coro/coro.asm
+++ samples/linux32o/coro/coro.asm
@@ -1,3 +1,6 @@
+;***************************************************************************
+; Source: https://github.com/spc476/C-Coroutines
+
 format ELF
 
 section '.text' executable
@@ -7,14 +10,14 @@ L_fun =	-8
 C_param	= -12
 
 start_it_up:
-		push	eax
-		push	dword [ebp + L_co]
-		call	dword [ebp + L_fun]
+	push eax
+	push dword [ebp + L_co]
+	call dword [ebp + L_fun]
 
 do_it_again:
-		mov	[ebp + C_param],eax
-		call	coroutine_yield32
-		jmp	do_it_again
+	mov	[ebp + C_param],eax
+	call coroutine_yield32
+	jmp	 do_it_again
 
 ;===========================================================================
 
@@ -24,8 +27,8 @@ P_co = 4
 
 public coroutine_init32
 coroutine_init32:
-		mov	edx,[esp + P_co]
-		mov	eax,[esp + P_stack]	; stack to switch to
+	mov edx,[esp + P_co]
+	mov eax,[esp + P_stack]	; stack to switch to
 
 	;------------------------------------------------------------
 	; Create the stack for resuming to start_it_up().  The stack
@@ -52,18 +55,18 @@ coroutine_init32:
 	; the stack, then restore EBP/ESP and "return" to start_it_up.
 	;------------------------------------------------------------
 
-		lea	ecx,[eax - 28]
-		mov	[ecx + 12],eax		; EBP of coroutine
-		mov	[ecx + 24],edx		; L_co
-		mov	eax,[esp + P_fun]	; L_fun
-		mov	[ecx + 20],eax
-		mov	dword [ecx + 16],start_it_up
-		xor	eax,eax
-		mov	[ecx + 8],eax		; "saved" EBX
-		mov	[ecx + 4],eax		; "saved" ESI
-		mov	[ecx + 0],eax		; "saved" EDI
-		mov	[edx],ecx		
-		ret
+	lea	ecx,[eax - 28]
+	mov	[ecx + 12],eax		; EBP of coroutine
+	mov	[ecx + 24],edx		; L_co
+	mov	eax,[esp + P_fun]	; L_fun
+	mov	[ecx + 20],eax
+	mov	dword [ecx + 16],start_it_up
+	xor	eax,eax
+	mov	[ecx + 8],eax		; "saved" EBX
+	mov	[ecx + 4],eax		; "saved" ESI
+	mov	[ecx + 0],eax		; "saved" EDI
+	mov	[edx],ecx		
+	ret
 
 ;===========================================================================
 
@@ -72,20 +75,20 @@ P_co = 4 + 16
 
 public coroutine_yield32
 coroutine_yield32:
-		push	ebp			; save callee saved registers
-		push	ebx
-		push	esi
-		push	edi
+	push ebp			; save callee saved registers
+	push ebx
+	push esi
+	push edi
 
-		mov	eax,[esp + P_param]	; return parameter
-		mov	edx,[esp + P_co]	; get stack to yield to
-		xchg	esp,[edx]		; YIELD!
+	mov eax,[esp + P_param]	; return parameter
+	mov edx,[esp + P_co]	; get stack to yield to
+	xchg esp,[edx]		; YIELD!
 
-		pop	edi			; retore registers
-		pop	esi
-		pop	ebx
-		pop	ebp
-		ret
+	pop	edi			; retore registers
+	pop	esi
+	pop	ebx
+	pop	ebp
+	ret
 
 ;***************************************************************************
 
