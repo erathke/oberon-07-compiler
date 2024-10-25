1d91eff2ae0d1db9f7eb997317cb6887ef6952ef
Evandro Rathke <evandro@erdev.tech>
2024-10-25 17:31:51 -0300

    samples: setjmp in progress

---
+++ samples/tmp/setjmp/setjmp.asm
@@ -0,0 +1,36 @@
+format ELF
+
+section '.text' executable
+
+public setjmp
+public longjmp
+
+
+setjmp:
+	mov eax, [esp + 4]
+	mov [eax +  0], ebx
+	mov [eax +  4], esi
+	mov [eax +  8], edi
+	mov [eax + 12], ebp
+	lea ecx, [esp + 4]
+	mov [eax + 16], ecx
+	mov ecx, [esp]
+	mov [eax + 20], ecx
+	xor eax, eax
+	ret
+
+
+longjmp:
+	mov edx, [esp + 4]
+	mov eax, [esp + 8]
+	cmp eax, 1
+	adc al, 0
+	mov ebx, [edx +  0]
+	mov esi, [edx +  4]
+	mov edi, [edx +  8]
+	mov ebp, [edx + 12]
+	mov esp, [edx + 16]
+	jmp dword [edx + 20]
+	ret
+
+section '.note.GNU-stack'
---
+++ samples/tmp/setjmp/test2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OBC="../../../compilerX"
+FASM="../../../fasm"
+LD="ld"
+DEF="-def API_sys -def API_TRAP"
+OPTS="-entry _start -rtl RTL_sys"
+BINDIR="bin"
+LIBDIR="lib"
+
+mkdir -p $BINDIR $LIBDIR;
+
+$FASM setjmp.asm $LIBDIR/setjmp.o &&
+$OBC $1.ob07 linux32o -out $LIBDIR/$1.xo $DEF $OPTS &&
+	$LD -T./linker.ld $LIBDIR/$1.xo $LIBDIR/setjmp.o -o bin/$1
+
