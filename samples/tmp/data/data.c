6ae242ee221db28b9ec40cebbf626b6f08b08dda
Evandro Rathke <evandro@erdev.tech>
2024-11-14 12:17:37 -0300

    samples: data to C tests

--- samples/tmp/data/data.c
+++ samples/tmp/data/data.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <assert.h>
 
 typedef struct {
+	char name[32];
 	int age, m1, m2;
 } Person;
 
 
-void update3(int size, const char *str) {
-	puts("--------------------------------");
-	printf("str: %s\n", str);
+void update2(Person *p) {
+	puts("---------------update2-----------------");
+	printf("person size: %u\n", sizeof(Person));
+	printf("person.age: %d\n", p->age);
+	printf("person.m1: %d\n", p->m1);
+	printf("person.m2: %d\n", p->m2);
 }
 
-
 void update4(int type, Person *p) {
-//void update4(int type, Person2 p) {
-	puts("--------------------------------");
+	puts("---------------update4-----------------");
 	printf("person type: %u\n", type);
 	printf("person.age: %d\n", p->age);
 	printf("person.m1: %d\n", p->m1);
@@ -22,10 +23,24 @@ void update4(int type, Person *p) {
 }
 
 void update5(int size, Person *p) {
-//void update4(int type, Person2 p) {
-	puts("--------------------------------");
+	puts("---------------update5-----------------");
 	printf("person size: %u\n", size);
+	printf("person.age: %s\n", p->name);
 	printf("person.age: %d\n", p->age);
 	printf("person.m1: %d\n", p->m1);
 	printf("person.m2: %d\n", p->m2);
+}
+
+
+void updateName(Person *p, int size, const char *name) {
+	strncpy(p->name, name, size);
+}
+
+void update7(Person *p) {
+	puts("---------------update7-----------------");
+	printf("person.name: %s\n", p->name);
+	printf("person.age: %d\n", p->age);
+	printf("person.m1: %d\n", p->m1);
+	printf("person.m2: %d\n", p->m2);
+	p->m2 = 876;
 }
