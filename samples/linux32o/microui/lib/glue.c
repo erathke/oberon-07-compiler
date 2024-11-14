099c726fad21908907c0249c8888154ff4ccb931
Evandro Rathke <evandro@erdev.tech>
2024-11-14 15:32:53 -0300

    samples: microui update

--- samples/linux32o/microui/lib/glue.c
+++ samples/linux32o/microui/lib/glue.c
@@ -1,41 +1,7 @@
 #include <stdlib.h>
 
-#include "raylib.h"
 #include "microui.h"
 #include "murl.h"
-
-// Raylib
-void _DrawCircle(int x, int y, double r, Color* color) {
-	DrawCircle(x, y, r, *color);
-}
-
-void _DrawRectangle(int x, int y, int w, int h, Color* color) {
-	DrawRectangle(x, y, w, h, *color);
-}
-
-void _ClearBackground(Color* color) {
-	ClearBackground(*color);
-}
-
-void _DrawText(const char *text, int x, int y, int font, Color* color) {
-	DrawText(text, x, y, font, *color);
-}
-
-Font* _CreateFont() {
-	return malloc(sizeof(Font));
-}
-
-void _LoadFont(const char *fileName, Font *font) {
-	*font = LoadFont(fileName);
-}
-
-void _LoadFontEx(const char *fileName, Font *font, int fontSize, int *codepoints, int codepointCount) {
-	*font = LoadFontEx(fileName, fontSize, codepoints, codepointCount);
-}
-
-void _DestroyFont(Font *font) {
-	free(font);
-}
 
 // Microui
 
