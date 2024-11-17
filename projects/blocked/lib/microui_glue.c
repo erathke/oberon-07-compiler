0b341cd0a3a13525e6260a4f7cd51c0736631b93
Evandro Rathke <evandro@erdev.tech>
2024-11-17 13:22:18 -0300

    projects: blocked in progress

--- projects/blocked/lib/microui_glue.c
+++ projects/blocked/lib/microui_glue.c
@@ -38,6 +38,7 @@ int _mu_multitext(mu_Context *ctx, int maxCols, int textLen, char *text, int _, 
 		res |= MU_RES_CHANGE;
 	}
 	if (ctx->focus == id) {
+		// insert characters
 		int len = strlen(ctx->input_text);
 		if (len > 0) {
 			int i = 0;
@@ -47,36 +48,33 @@ int _mu_multitext(mu_Context *ctx, int maxCols, int textLen, char *text, int _, 
 				res |= MU_RES_CHANGE;
 				i++;
 				cursorPos->x++;
-			}
-			if (cursorPos->x >= maxCols){
-				cursorPos->y++;
-				cursorPos->x = 0;
+				if (cursorPos->x >= maxCols){
+					cursorPos->y++;
+					cursorPos->x = 0;
+				}
 			}
 		}
-		
+		// remove characters
 		if (ctx->key_pressed & MU_KEY_BACKSPACE) {
-			
 			cursorPos->x--;
-			
-			if (cursorPos->x <= 0){
+			if (cursorPos->x < 0){
 				if (cursorPos->y > 0) {
 					cursorPos->y--;
-					cursorPos->x = maxCols;
+					cursorPos->x = maxCols - 1;
 				} else {
 					cursorPos->x = 0;
 					cursorPos->y = 0;
 				}
 			}
-			
 			int tpos = cursorPos->x + (cursorPos->y * maxCols); 
 			text[tpos] = 0;
 			res |= MU_RES_CHANGE;
 		}
-		
+		// submit changes
 		if (ctx->key_pressed & MU_KEY_RETURN) {
 			mu_set_focus(ctx, 0);
 			res |= MU_RES_SUBMIT;
-		}	
+		}
 	}
 	
 	/* draw */
@@ -84,30 +82,40 @@ int _mu_multitext(mu_Context *ctx, int maxCols, int textLen, char *text, int _, 
 	mu_Color panelColor = {18, 18, 18, 255};
 	mu_Color textColor = {255, 0, 0, 255};
 	mu_Vec2 posText = {rect.x + 5, rect.y + 10};
+	const int lineHeight = ctx->text_height(font) + 5;
 	mu_draw_rect(ctx, rect, panelColor);
 	
-	int len = strlen(text);	
-	char row[maxCols];
+	// Drawing text
 	for (int i = 0;  i <= cursorPos->y; i++) {
-		strncpy(row, text + (i * maxCols), maxCols);
-		mu_draw_text(ctx, font, row, maxCols, posText, textColor);
-		posText.y += 20;
+		const char *colOffs = text + (i * maxCols); 
+		mu_draw_text(ctx, font, colOffs, maxCols, posText, textColor);
+		posText.y += lineHeight;
 	}
 	
 	if (ctx->focus == id)  {	
 		mu_Color cursorColor = {255, 255, 0, 255};
-		const int px = 8;
-		const int py = 20;
+		int px = ctx->text_width(font, "#", 1) + 1;
+		const int py = lineHeight;
 		int x = rect.x + (cursorPos->x * px) + 3;
 		int y = rect.y + (cursorPos->y * py) + 5;
-		mu_Rect cursorRect = {x, y, 2, py};
+		mu_Rect cursorRect = {x, y + 2, 2, py};
 		mu_Rect maxPosRect = {rect.x + (maxCols * px) + 6, rect.y + 1, 1, rect.h - 2};
 		
-		mu_draw_rect(ctx, cursorRect, cursorColor);
 		mu_draw_box(ctx, rect, textColor);
+		mu_draw_rect(ctx, cursorRect, cursorColor);
 		cursorColor.a = 100;
 		mu_draw_rect(ctx, maxPosRect, cursorColor);
 	} 
 
 	return res;
+}
+
+int _mu_textwidth(mu_Context *ctx, int textLen, const char *text) {
+	mu_Font font = ctx->style->font;
+	return ctx->text_width(font, text, textLen);
+}
+
+int _mu_textheight(mu_Context *ctx) {
+	mu_Font font = ctx->style->font;
+	return ctx->text_height(font);
 }
